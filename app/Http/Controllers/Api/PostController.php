611f06ea07d17006ffdea6522585ab32715523d8
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-15 19:01:50 +0100

    creo una response per ricevere i dati tramite API dal DB

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        
+
+        $post = Post::all();
+
+        return response()->json( compact('post') );
     }
 
     
