57bc72fd16715ca38d9080813f5ce5c04fc97380
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-15 15:48:59 +0100

    first commit

---
+++ database/seeds/TravelTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Travel;
+
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
+
+class TravelTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+    }
+}
