36fe10d2c68e285c41a219d97ebe6c120cd26e21
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-17 16:46:21 +0100

    creazione seeder Users + creazione model UserInfo e rel. migrat. + add foreign key user_id tra user e userInfo

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function info(){
+
+        return $this->hasOne('App\Models\UserInfo', 'user_id');
+
+    }
 }
