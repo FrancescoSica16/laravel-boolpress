4a50337680fbbf2fb490b858d3a920df3cd91042
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-17 19:27:20 +0100

    aggiunti dati user_info in db

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\UserInfo;
 use Illuminate\Database\Seeder;
 
 use App\User;
@@ -17,12 +18,23 @@ class UsersTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i=0; $i < 10 ; $i++) { 
+            //creo utente
             $newUser = new User();
             
             $newUser->name = $faker->userName();
             $newUser->email = $faker->safeEmail();
             $newUser->password = $faker->password(9,14);
             $newUser->save();
+
+            //creo user info
+            $newUserInfo = new UserInfo();
+            $newUserInfo->user_id = $newUser->id;
+            $newUserInfo->address = $faker->address();
+            $newUserInfo->contry = $faker->country();
+            $newUserInfo->phone = $faker->phoneNumber();
+
+            $newUserInfo->save();
+
         }
 
         $user = new User();
