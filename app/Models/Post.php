b12ff76ad08773eae526ecf5cf633c6536c1a2b7
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-19 16:53:46 +0100

    definizione foreign_key in post_tag / inserimento nei relativi modelli delle foreign key

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,12 @@ class Post extends Model
     public function category(){  
         return $this->BelongsTo('App\Models\Category');
     }
+
+    public function user(){
+        return $this->belongsTo('App\User');  // mi servirà per user_info
+    }
+
+    public function tags(){
+        return $this->belongsToMany('App\Models\Tag');
+    }
 }
