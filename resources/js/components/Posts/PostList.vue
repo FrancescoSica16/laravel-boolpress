7e84abbb9a5073a63d0c0d7842cd1cf45b93c2c7
FrancescoSica16 <francesco.sica16@gmail.com>
2021-11-23 19:21:16 +0100

    definizione component principali + aggiunta window.axios = require('axios'); in front.js per  importare axios

---
+++ resources/js/components/Posts/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <section id="post-list" class="">
+      <h2>I miei post</h2>
+
+  </section>
+</template>
+
+<script>
+
+export default {
+    name: 'postList',
+    data(){
+        return{
+            posts : [],
+
+        }
+    },
+    methods:{
+        getPostList(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/front.js
+++ resources/js/front.js
@@ -1,5 +1,7 @@
 window.Vue = require('vue');
 
+window.axios = require('axios');
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -11,7 +13,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -19,7 +21,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-import App from "./components/App.Vue";
+import App from "./components/App.vue";
  
 const app = new Vue({
     el: '#root',
